2997576d7ce4d360de740bf60256d4d38f32dab0
Mark Blaze <github@markblaze.dev>
2025-11-11 08:53:43 -0500

    clean up

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,8 +79,8 @@ const sr = ScrollReveal({
 
 sr.reveal('.home__data, .explore__data, .explore__user, .footer__container')
 sr.reveal('.home__card', {delay: 600, distance: '100px', interval: 100})
-sr.reveal('.about__data, .join__image', {origin: 'right'})
-sr.reveal('.about__image, .join__data', {origin: 'left'})
+sr.reveal('.about__data, .final-project__image', {origin: 'right'})
+sr.reveal('.about__image, .final-project__data', {origin: 'left'})
 sr.reveal('.project__card', {interval: 200})
 
 /*=============== AUTO COPYRIGHT YEAR UPDATE ===============*/
